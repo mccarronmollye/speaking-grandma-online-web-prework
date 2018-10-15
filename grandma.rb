46bc69cf65ec2b9bd5e6e6c677befdb7b3c9edb8
Molly McCarron <mccarronmollye@gmail.com>
2018-10-15 11:35:21 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
-if string == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
-end
+  if string == "I LOVE YOU GRANDMA!"
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).

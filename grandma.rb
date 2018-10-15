aba14d22b73a976b08e23e4d7470b107a939af50
Molly McCarron <mccarronmollye@gmail.com>
2018-10-15 11:34:46 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,6 +2,7 @@
 def speak_to_grandma(string)
 if string == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
+end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).

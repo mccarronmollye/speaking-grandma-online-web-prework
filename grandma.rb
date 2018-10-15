1ddadb1f4134b5ae52221b788723987ab39c376f
Molly McCarron <mccarronmollye@gmail.com>
2018-10-15 11:43:26 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
-  if string == string.upcase
-    "NO, NOT SINCE 1938!"
-  elsif string == "I LOVE YOU GRANDMA!"
+if string == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
+elsif string == string.upcase
+    "NO, NOT SINCE 1938!"
 end
 end
 # Whatever you say to grandma, she should respond with

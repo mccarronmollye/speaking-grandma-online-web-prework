b657a46f48a96231f9bbe8c9c0d3d967b493a154
Molly McCarron <mccarronmollye@gmail.com>
2018-10-15 11:42:26 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,6 +2,8 @@
 def speak_to_grandma(string)
   if string == string.upcase
     "NO, NOT SINCE 1938!"
+  elsif string == "I LOVE YOU GRANDMA!"
+    "I LOVE YOU TOO PUMPKIN!"
 end
 end
 # Whatever you say to grandma, she should respond with

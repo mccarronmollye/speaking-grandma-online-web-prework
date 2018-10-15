58d08ffe832c31b3be5848bdf27260ec4fd17837
Molly McCarron <mccarronmollye@gmail.com>
2018-10-15 10:49:34 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,15 +1,15 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
   puts (phrase)
-end 
+end
 
-else 
+else
   puts
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
